40db5e738cd7d41aedf4acc41ada9aa01d42f1a2
Ryan Rehan <ryanrehan.pc@gmail.com>
2026-01-31 21:15:25 +0600

    updating the app

--- src/app.js
+++ src/app.js
@@ -4,12 +4,20 @@ import routes from "./routes/ index.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", routes);
 
 app.use(errorMiddleware);
+
+app.get("/", (_, res) => {
+  res.send("Saiyara Overseas API running");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -2,14 +2,7 @@ import "dotenv/config"
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-const PORT = process.env.PORT || 5000;
+
 
 connectDB();
 
-app.get("/", (_, res) => {
-  res.send("Saiyara Overseas API running");
-});
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
